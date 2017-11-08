d147a395aece21d497c57df95fbdb41842e7c271
ivany2013 <491025640@qq.com>
2017-11-08 11:50:59 +0800

    first commit

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,25 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by mysteel-xl on 2017/11/7.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = simpleDateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mysteel-xl
+  Date: 2017/11/7
+  Time: 14:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<h1>${currentTime}</h1>
+</body>
+</html>
